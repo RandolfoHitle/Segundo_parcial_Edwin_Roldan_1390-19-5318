6f8737c63034374a0cc3b5a7b645c15d22b64f72
EdwinRan <eroldanp@miumg.edu.gt>
2024-09-28 15:53:06 -0600

    supuesto todo contenido del examen juas juas2

--- segundo_proyecto/urls.py
+++ segundo_proyecto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from gestion.views import ProfesorViewSet, MascotaViewSet
 
+router = routers.DefaultRouter()
+router.register(r'profesores', ProfesorViewSet)
+router.register(r'mascotas', MascotaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
